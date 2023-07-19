8cca78df4dbc962f5c04ad5fa06b23b1f5c67abd
Md: Mohon <mdmohon6145@gmail.com>
2023-07-20 01:11:27 +0600

    wishlist endpoint added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { BookRoutes } from '../modules/book/book.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { WishListRoutes } from '../modules/wishlist/wishlist.route';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/book',
     route: BookRoutes,
+  },
+  {
+    path: '/wishlist',
+    route: WishListRoutes,
   },
 ];
 
